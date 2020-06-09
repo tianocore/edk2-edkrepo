fa34503c285a2ed74e893eb7ab66f85f88f51a45
Nate DeSimone <nathaniel.l.desimone@intel.com>
2020-06-08 17:08:12 -0700

    EdkRepo: Fix argument checks in manifest-repos
    Fixed a logic bug in the argument checking for the
    manifest-repos command.
    
    Signed-off-by: Nate DeSimone <nathaniel.l.desimone@intel.com>
    Cc: Ashley E Desimone <ashley.e.desimone@intel.com>
    Cc: Puja Pandya <puja.pandya@intel.com>
    Cc: Bret Barkelew <Bret.Barkelew@microsoft.com>
    Cc: Prince Agyeman <prince.agyeman@intel.com>
    Cc: Erik Bjorge <erik.c.bjorge@intel.com>
    Reviewed-by: Ashley DeSimone <ashley.e.desimone@intel.com>
    Reviewed-by: Erik Bjorge <erik.c.bjorge@intel.com>

--- edkrepo/commands/humble/manifest_repos_humble.py
+++ edkrepo/commands/humble/manifest_repos_humble.py
@@ -14,7 +14,7 @@ Contains user visible strings printed by the manifest_repos command.
 CFG_LIST_ENTRY = 'Config File: edkrepo.cfg Manifest Repository Name: {}'
 USER_CFG_LIST_ENTRY = 'Config File: edkrepo_user.cfg Manifest Repository Name: {}'
 NAME_REQUIRED = 'The "name" argument is required to add/remove a manifest repository'
-ADD_REQUIRED = 'The "name", "url", "branch" and "local-path" arguments are required to add a manifest repository'
+ADD_REQUIRED = 'The "name", "url", "branch" and "path" arguments are required to add a manifest repository'
 CANNOT_REMOVE_CFG = 'Manifest repositories cannot be removed from the edkrepo.cfg file.'
 REMOVE_NOT_EXIST = 'The selected manifest repository does note exist in the edkrepo_user.cfg file.'
 ALREADY_EXISTS = 'A manifest repository already exists with name: {}'
