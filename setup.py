d8271bdf49d3fe19bc4dc468db72854fe7f455cd
Nate DeSimone <nathaniel.l.desimone@intel.com>
2021-05-20 15:31:00 -0700

    Increment version to 2.0.5
    Signed-off-by: Nate DeSimone <nathaniel.l.desimone@intel.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 from setuptools import setup
 
 setup(name='edkrepo',
-      version='2.0.4',
+      version='2.0.5',
       description='The edkrepo tools',
       packages=['edkrepo', 'edkrepo.commands', 'edkrepo.commands.arguments', 'edkrepo.commands.humble',
                 'edkrepo.git_automation', 'edkrepo.common', 'edkrepo.common.workspace_maintenance',
