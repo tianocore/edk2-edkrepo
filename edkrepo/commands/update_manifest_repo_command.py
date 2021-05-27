638353513a518413938124a2783fec1c5d560191
Ashley E Desimone <ashley.e.desimone@intel.com>
2021-05-26 21:12:16 -0700

    EdkRepo: Add the update_manifest_repo command
    Adds functionality to update the copy of the global
    manifest repo(s) used by EdkRepo
    
    Signed-off-by: Ashley E Desimone <ashley.e.desimone@intel.com>

---
+++ edkrepo/commands/update_manifest_repo_command.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#
+## @file
+# update_manifest_repo_command.py
+#
+# Copyright (c) 2017 - 2021, Intel Corporation. All rights reserved.<BR>
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+
+from edkrepo.commands.edkrepo_command import EdkrepoCommand
+from edkrepo.commands.edkrepo_command import DryRunArgument
+from edkrepo.common.workspace_maintenance.manifest_repos_maintenance import pull_all_manifest_repos
+import edkrepo.commands.arguments.update_manifest_repo as arguments
+
+class UpdateManifestRepoCommand(EdkrepoCommand):
+    def __init__(self):
+        super().__init__()
+
+    def get_metadata(self):
+        metadata = {}
+        metadata['name'] = 'update-manifest-repo'
+        metadata['help-text'] = arguments.UPDATE_MANIFEST_REPO_COMMAND_DESCRIPTION
+        metadata['alias'] = 'umr'
+        args = []
+        metadata['arguments'] = args
+        args.append({'name' : 'hard',
+                     'positional' : False,
+                     'required' : False,
+                     'description' : arguments.UPDATE_MANIFEST_REPO_HARD_DESCRIPTION,
+                     'help-text' : arguments.UPDATE_MANIFEST_REPO_HARD_HELP})
+        return metadata
+
+    def run_command(self, args, config):
+        pull_all_manifest_repos(config['cfg_file'], config['user_cfg_file'], reset_hard=args.hard)
