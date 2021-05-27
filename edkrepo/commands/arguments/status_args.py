784ff89b279010276334b140e629e7e0dd2cb803
Ashley E Desimone <ashley.e.desimone@intel.com>
2021-05-26 20:52:18 -0700

    EdkRepo: Add the status command
    Provides functionality equivalent to running git
    status for all repositories in the workspace.
    Additionally displays the currently checked out
    combo
    
    Signed-off-by: Ashley E Desimone <ashley.e.desimone@intel.com>

---
+++ edkrepo/commands/arguments/status_args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#
+## @file
+# status_args.py
+#
+# Copyright (c) 2020 - 2021, Intel Corporation. All rights reserved.<BR>
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+
+''' Contains the help and description strings for arguments in the
+sync command meta data.
+'''
+
+#Args for status_command.py
+STATUS_COMMAND_DESCRIPTION = ('Displays the current combo and the status of each repository in the combination.')
