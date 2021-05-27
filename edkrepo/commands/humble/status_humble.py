07ffc92c73d21fba78bcdc2d8aac991a15b9fd09
Ashley E Desimone <ashley.e.desimone@intel.com>
2021-05-26 20:52:18 -0700

    EdkRepo: Add the status command
    Provides functionality equivalent to running git
    status for all repositories in the workspace.
    Additionally displays the currently checked out
    combo
    
    Signed-off-by: Ashley E Desimone <ashley.e.desimone@intel.com>

---
+++ edkrepo/commands/humble/status_humble.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#
+## @file
+# status_humble.py
+#
+# Copyright (c) 2020 - 2021, Intel Corporation. All rights reserved.<BR>
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+
+''' Contains informational and error messages outputted by
+the run_command functions of Edkrepo commands
+'''
+
+#Messages for status_command.py
+STATUS_CURRENT_COMBO = 'Current combo: {}\n'
