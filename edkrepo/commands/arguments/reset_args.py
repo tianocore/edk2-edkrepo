815a681da49014ce79ace8734094796d7729e699
Ashley E Desimone <ashley.e.desimone@intel.com>
2021-05-26 20:41:14 -0700

    EdkRepo: Add the reset command
    The reset command provides the same functionality
    as running git reset on all repositories in the
    workspace.
    
    Signed-off-by: Ashley E Desimone <ashley.e.desimone@intel.com>

---
+++ edkrepo/commands/arguments/reset_args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+## @file
+# reset_command.py
+#
+# Copyright (c) 2020 - 2021, Intel Corporation. All rights reserved.<BR>
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+
+''' Contains the help and description strings for arguments in the
+combo command meta data.
+'''
+
+#Args for reset_command.py
+RESET_COMMAND_DESCRIPTION = 'Unstages all staged files in the workspace'
+HARD_HELP = 'Deletes files after unstaging'
