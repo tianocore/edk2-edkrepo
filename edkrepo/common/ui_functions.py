05e2ade57458cc57e973beaba50d46cb0eb109e7
Isil Berkun <isil.berkun@intel.com>
2021-06-08 18:38:06 -0700

    colorama.init() moves to the init_color_console method and is being called from related print methods

--- edkrepo/common/ui_functions.py
+++ edkrepo/common/ui_functions.py
@@ -14,6 +14,10 @@ import string
 import git
 import colorama
 
+from colorama import Fore
+from colorama import Style 
+from colorama import init
+
 from edkrepo.common.pathfix import expanduser
 
 def init_color_console(force_color_output):
@@ -29,7 +33,7 @@ def init_color_console(force_color_output):
     if os.name == 'posix':
         # Never convert on Linux.  Setting it to False seems to cause problems.
         convert=None
-    colorama.init(strip=strip, convert=convert)
+    colorama.init(strip=strip, convert=convert, autoreset=True)
     return strip, convert
 
 
@@ -46,6 +50,39 @@ def display_git_output(output_data, verbose=False):
         print(output_data[1])
     if verbose and output_data[2]:
         print(output_data[2])
+
+def print_info_msg(info_msg, header=True):
+    """
+    Displays informational message with (default) or without header. 
+    """
+    init_color_console("init")
+    if header:
+        info_msg_formatted = "{}{}Info: {}{}{}".format(Style.BRIGHT, Fore.CYAN, Style.RESET_ALL, Fore.CYAN, info_msg)
+    else:
+        info_msg_formatted = "{}{}".format(Fore.CYAN, info_msg)
+    print(info_msg_formatted)
+
+def print_warning_msg(warning_msg, header=True):
+    """
+    Displays warning message with (default) or without header.
+    """
+    init_color_console("init")
+    if header:
+        warning_msg_formatted = "{}{}Warning: {}{}{}".format(Style.BRIGHT, Fore.YELLOW, Style.RESET_ALL, Fore.YELLOW, warning_msg)
+    else:
+        warning_msg_formatted = "{}{}".format(Fore.YELLOW, warning_msg)
+    print(warning_msg_formatted)
+
+def print_error_msg(error_msg, header=True):
+    """
+    Displays error message with (default) or without header.
+    """
+    init_color_console("init")
+    if header:
+        error_msg_formatted = "{}{}Error: {}{}{}".format(Style.BRIGHT, Fore.RED, Style.RESET_ALL, Fore.RED, error_msg)
+    else:
+        error_msg_formatted = "{}{}".format(Fore.RED, error_msg)
+    print(error_msg_formatted)
 
 def print_safe(input_string):
     print(safe_str(input_string))
