4e8906c6eb8df4981da20e6bad23376e386ffa8b
Ashley E Desimone <ashley.e.desimone@intel.com>
2021-11-03 13:15:43 -0700

    EdkRepo: Add list pins command
    This command provides support for listing all pin files in the
    global manifest repository.
    
    Signed-off-by: Ashley E Desimone <ashley.e.desimone@intel.com>
    Reviewed-by: Nate DeSimone <nathaniel.l.desimone@intel.com>

---
+++ edkrepo/commands/arguments/list_pins_args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+## @file
+# list_pins_args.py
+#
+# Copyright (c) 2020-2021, Intel Corporation. All rights reserved.<BR>
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+''' Contains the help and description strings for arguments in the
+list-repos command meta data.
+'''
+
+#Args for list_pins_command.py
+LIST_PINS_COMMAND_DESCRIPTION = 'List pin files available for the current project'
+LIST_PINS_DESCRIPTION_ARG_DESC = 'Prints the description field of the pin file in addition to the pin file name'
+LIST_PINS_PROJECT_DESCRIPTION = 'The project to display pin files for'
---
+++ edkrepo/commands/humble/list_pins_humble.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#
+## @file
+# list_pins_humble.py
+#
+# Copyright (c) 2020-2021, Intel Corporation. All rights reserved.<BR>
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+
+''' Contains informational and error messages outputted by
+the run_command functions of Edkrepo commands
+'''
+
+# Debug messages for list_pins_command.py
+PIN_FOLDER = 'Pin folder:\n  {}\n'
+NOT_IN_WKSPCE = 'Not currently in a valid workspace. The --project flag must be used.'
+NO_PIN_FOLDER = 'No pin folder defined in manifest file.'
