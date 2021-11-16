b87202d2457276cb601ebc3c66cf5977c6270bda
Ashley E Desimone <ashley.e.desimone@intel.com>
2021-11-15 16:59:23 -0800

    EdkRepo: Deprecate create-pin --push
    Direct push functionality is not supported by most
    access controlled git environments and oftentimes creates
    non-linear histories when not desired. As such remove the
    direct push functionality until there is a defined requirement
    or use case which warrants it.
    
    Signed-off-by: Ashley E Desimone <ashley.e.desimone@intel.com>
    Reviewed-by: Nate DeSimone <nathaniel.l.desimone@intel.com>

--- edkrepo/commands/arguments/create_pin_args.py
+++ edkrepo/commands/arguments/create_pin_args.py
@@ -15,7 +15,4 @@ COMMAND_DESCRIPTION = 'Creates a PIN file based on the current workspace state'
 NAME_HELP = ('The name of the PIN file. Extension must be .xml. File paths are '
              'supported only if the --push option is not used.')
 DESCRIPTION_HELP = 'A short summary of the PIN file contents. Must be contained in ""'
-PUSH_HELP = ('Automatically commit and push the PIN file to the global manifest '
-             'repository at the location specified by the Pin-Path field in '
-             'the project manifest file.')
 
