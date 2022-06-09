10aa390892a657d5121282b178592f865c7870b5
Harsh Vora <harsh.vora@intel.com>
2022-06-08 17:53:11 -0700

    Edkrepo: Update Caching to enable configurable cache locations
    Update the cache command to enable the --path argument
    Update the cache command to not write caching states / paths
    to the edkrepo_user.cfg when the --info flag is used.
    
    Add cache-path data to the edkrepo_user.cfg as well as get and set
    methods.
    
    Update common_cache_functions.py methods such that
    get_cache_directory pulls the cache path from the edkrepo_user.cfg
    
    Signed-off-by: Ashley E Desimone <ashley.e.desimone@intel.com>
    Signed-off-by: Harsh Vora harsh.vora@intel.com
    Reviewed-by: Kevin Sun <kevin.sun@intel.com>
    Reviewed-by: Liyu Ying <liyu.ying@intel.com>

--- edkrepo/common/common_cache_functions.py
+++ edkrepo/common/common_cache_functions.py
@@ -13,16 +13,19 @@ from edkrepo.config.config_factory import get_edkrepo_global_data_directory
 from edkrepo.config.tool_config import SUBMODULE_CACHE_REPO_NAME
 from project_utils.cache import RepoCache
 
-
-def get_global_cache_directory(config):
+def get_cache_directory(config):
     if config['user_cfg_file'].caching_state:
-        return os.path.join(get_edkrepo_global_data_directory(), '.cache')
-    return None
-
+        if config['user_cfg_file'].cache_path == 'default':
+            return os.path.join(get_edkrepo_global_data_directory(), '.cache')
+        else:
+            cache_path = os.path.abspath(os.path.normcase(os.path.normpath(config['user_cfg_file'].cache_path)))
+        return os.path.join(cache_path, '.cache')
+    else:
+        return None
 
 def get_repo_cache_obj(config):
     cache_obj = None
-    cache_directory = get_global_cache_directory(config)
+    cache_directory = get_cache_directory(config)
     if cache_directory is not None:
         cache_obj = RepoCache(cache_directory)
         cache_obj.open()
