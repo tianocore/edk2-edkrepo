5ed881934cd4f70b9b6bb86c09fe771c5ce96b75
Kevin Sun <kevin.sun@intel.com>
2022-08-15 14:18:24 -0700

    EdkRepo: Version-specific includeifs in .gitconfig
    Includeif updates consist of adding "%(prefix)" to all includeif entries
    in .gitconfig file but caused some regressions. Updating to make this
    update Git-version-specific as the update is only needed for Git
    versions 2.34.0 and higher.
    
    Signed-off-by: Kevin Sun <kevin.sun@intel.com>
    Reviewed-by: Ashley DeSimone <ashley.e.desimone@intel.com>

--- edkrepo/common/workspace_maintenance/git_config_maintenance.py
+++ edkrepo/common/workspace_maintenance/git_config_maintenance.py
@@ -14,17 +14,28 @@ import sys
 import git
 
 from edkrepo.common.pathfix import expanduser
+from edkrepo.common.common_repo_functions import find_git_version
+from edkrepo.common.git_version import GitVersion
 
 def clean_git_globalconfig():
     global_gitconfig_path = os.path.normpath(expanduser("~/.gitconfig"))
+    prefix_required = find_git_version() >= GitVersion('2.34.0')
     with git.GitConfigParser(global_gitconfig_path, read_only=False) as git_globalconfig:
-        includeif_regex = re.compile('^includeIf "gitdir:%\(prefix\)(/.+)/"$')
-        includeif_regex_old = re.compile('^includeIf "gitdir:(/.+)/"$')
+        if prefix_required:
+            includeif_regex = re.compile('^includeIf "gitdir:%\(prefix\)(/.+)/"$')
+            includeif_regex_old = re.compile('^includeIf "gitdir:(/.+)/"$')
+        else:
+            includeif_regex_old = re.compile('^includeIf "gitdir:%\(prefix\)(/.+)/"$')
+            includeif_regex = re.compile('^includeIf "gitdir:(/.+)/"$')
         for section in git_globalconfig.sections():
             data = includeif_regex.match(section)
             if data:
                 gitrepo_path = data.group(1)
                 gitconfig_path = git_globalconfig.get(section, 'path')
+                if _path_is_new_style(gitrepo_path):
+                    gitrepo_path = _remove_new_style_prefix(gitrepo_path)
+                if _path_is_new_style(gitconfig_path):
+                    gitconfig_path = _remove_new_style_prefix(gitconfig_path)
                 if sys.platform == "win32":
                     gitrepo_path = gitrepo_path[1:]
                     gitconfig_path = gitconfig_path[1:]
@@ -35,7 +46,6 @@ def clean_git_globalconfig():
                 if not os.path.isdir(gitrepo_path) and not os.path.isfile(gitconfig_path):
                     if not os.path.isfile(repo_manifest_path):
                         git_globalconfig.remove_section(section)
-            
             data_old = includeif_regex_old.match(section)
             if data_old:
                 git_globalconfig.remove_section(section)
@@ -46,3 +56,9 @@ def set_long_path_support():
         if 'core' not in git_globalconfig.sections():
             git_globalconfig.add_section('core')
         git_globalconfig.set('core', 'longpaths', 'true')
+
+def _path_is_new_style(path):
+    return path.startswith('%(prefix)')
+
+def _remove_new_style_prefix(path):
+    return path.split('%(prefix)')[1]
