6d6e3dab32b9968cdc8160e7c3081b33c9c2cc81
Ashley E Desimone <ashley.e.desimone@intel.com>
2023-10-20 15:42:57 -0700

    EdkRepo: Combo Command UX enhancement
    Add new method to ui_functions.py to print the current project and if verbose is selected the project description.
    
    Update the UX of the combo command to:
    -Display the current project
    -Add line breaks between logical sections of output
    -Add colored output
    -Add a non-color indicator "-" to archived projects
    -Display the combo description when verbose output is selected.
    
    Fixes #202
    
    Signed-off-by: Ashley E Desimone <ashley.e.desimone@intel.com>
    Reviewed-by:  Nate DeSimone <nathaniel.l.desimone@intel.com>

---
+++ edkrepo/commands/humble/combo_humble.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#
+## @file
+# combo_humble.py
+#
+# Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+
+'''
+Contains user visible strings printed by the list-repos command.
+'''
+
+from colorama import Fore
+from colorama import Style
+
+CURRENT_COMBO = "{}{}* {{}}{}".format(Fore.GREEN, Style.BRIGHT, Fore.RESET)
+ARCHIVED_COMBO = "{}{}- {{}}{}".format(Fore.YELLOW, Style.BRIGHT, Style.RESET_ALL)
+COMBO = "  {}{{}}{}".format(Style.BRIGHT, Style.RESET_ALL)
+COMBO_DESCRIPTION = "    {}Description: {{}}{}".format(Style.BRIGHT, Style.RESET_ALL)
+NO_DESCRIPTION = "{}no description included in combo definition{}".format(Fore.RED, Style.RESET_ALL)
+REPO_DETAILS = "    {}{}{{}}{} : {}{}{{}}{}".format(Style.BRIGHT, Fore.CYAN, Style.RESET_ALL, Style.BRIGHT, Fore.BLUE, Style.RESET_ALL)
