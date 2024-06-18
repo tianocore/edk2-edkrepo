2f7d81ab480812b50e20dd2a95c162b7bc6a7ff7
Nathaniel Haller <nathaniel.d.haller@intel.com>
2024-06-18 16:26:01 -0700

    Raise ImportError if no command-packages are successfully imported.
    If the cfg file contains command-packages section includes multiple packages,
    raise ImportError if all packages fail to import.
    
    GlobalConfig file command-packages can be expanded and remain testable with
    with Tianocore edk2-edkrepo edkrepo.commands.
    
    Signed-off-by: Nathaniel Haller <nathaniel.d.haller@intel.com>
    (cherry picked from commit d3a70f6fedc370ea262d9c53add01505a6b60614)
    (cherry picked from commit 8868a557d5407723f755cf0a50891d27d0833fa7)

---
+++ edkrepo/commands/humble/command_factory_humble.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+## @file
+# command_factory_humble.py
+#
+# Copyright (c) 2024, Intel Corporation.
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+
+COMMAND_PACKAGE_NOT_FOUND = 'command-package {} defined in the cfg file was not found.'
+COMMAND_PACKAGES_NOT_FOUND = 'No command-packages defined in the cfg file were found.'
