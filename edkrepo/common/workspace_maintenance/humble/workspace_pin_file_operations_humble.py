52a6dec208fbf8e92062cc574198d62f22414d07
Robert Plascencia <robert.plascencia0@gmail.com>
2026-04-02 12:26:50 -0700

    Remove circular dependency
    extract `get_checked_out_pin_file` to `workspace_pin_file_operations.py`
    
    Fixes #409
    Signed-off-by: Robert Plascencia <robert.plascencia0@gmail.com>

---
+++ edkrepo/common/workspace_maintenance/humble/workspace_pin_file_operations_humble.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#
+## @file
+# workspace_pin_file_operations_humble.py
+#
+# Copyright (c) 2026, Intel Corporation. All rights reserved.<BR>
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+
+''' Contains user facing strings for workspace_pin_file_operations.py '''
+
+PIN_FILE_NOT_FOUND = 'Pin file {} not found in manifest repository or workspace root'
